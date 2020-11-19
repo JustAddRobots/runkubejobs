3f8515c78f96141b34f2ccc2733dfa9f7ae2b5f2
JustAddRobots <justaddrobots@icloud.com>
2020-11-18 22:55:57 -0800

    stage: Added loading kubernetes config from within cluster

--- runkubejobs/cli.py
+++ runkubejobs/cli.py
@@ -212,7 +212,8 @@ def run(d):
     my_cli.print_versions()
 
     # Setup Kubernetes config and API
-    config.load_kube_config()
+    # config.load_kube_config()
+    config.load_incluster_config()
     core = client.CoreV1Api()
     batch = client.BatchV1Api()
     w = watch.Watch()
